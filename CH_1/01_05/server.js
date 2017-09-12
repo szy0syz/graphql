12c74154c9ef8ef1cd8395b21c1bcee18a47f472
szy0syz <szy0syz@gmail.com>
2017-09-12 23:59:58 +0800

    updated 01_05

---
+++ CH_1/01_05/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import graphqlHTTP from 'express-graphql'
+import schema from './schema';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('GraphQL & Relay modern is cool!!!');
+});
+
+const root = { friend: () => {
+  return {
+      "id": 9527,
+      "firstName": "Jerry",
+      "lastName": "Shi",
+      "gender": "Male",
+      "language": "Chinexe",
+      "email": "szy0syz@gmail.com",
+  }
+}};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}));
+
+app.listen(8080, () => console.log('Running server on localhost:8080/graphql'));
